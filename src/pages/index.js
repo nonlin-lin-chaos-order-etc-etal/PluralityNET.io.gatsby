6f4cbf91deee7f271b35d5cff82de0b231f635d7
nonlinear-chaos-order-etc-etal <19966907+nonlinear-chaos-order-etc-etal@users.noreply.github.com>
2021-02-28 23:55:14 +0800

    take 1 at logo svg

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,40 +15,11 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>PluralityNET</title>
       </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <img src="/src/images/logo/vector/default.svg"/>
+      <hr noshade/>
+      <p>Copyright © {date ? date : 'Loading date...'} PluralityNET.io. All Rights Reserved.</p>
     </main>
   );
 }
